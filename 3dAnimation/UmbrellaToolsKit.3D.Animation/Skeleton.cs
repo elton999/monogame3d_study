7b0351073437d4b66eae25d8faf5faa403720ab4
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-18 12:23:57 -0300

    added Skeleton class

---
+++ 3dAnimation/UmbrellaToolsKit.3D.Animation/Skeleton.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace UmbrellaToolsKit.Animation3D
+{
+    public class Skeleton
+    {
+        protected Pose mRestPose;
+        protected Pose mBindPose;
+        protected List<Matrix> mInvBindPose;
+        protected List<string> mJointNames;
+
+        public Skeleton() { }
+
+        public Skeleton(Pose rest, Pose bind, List<string> names) => Set(rest, bind, names);
+
+        public void Set(Pose rest, Pose bind, List<string> names)
+        {
+            mRestPose = rest;
+            mBindPose = bind;
+            mJointNames = names;
+            UpdateInverseBindPose();
+        }
+
+        public Pose GetBindPose() => mBindPose;
+
+        public Pose GetRestPose() => mRestPose;
+
+        public List<Matrix> GetInvBindPose() => mInvBindPose;
+
+        public List<string> GetJoinNames() => mJointNames;
+
+        public string GetJoinName(int index) => mJointNames[index];
+
+        protected void UpdateInverseBindPose()
+        {
+            int size = mBindPose.Size();
+            mInvBindPose.Capacity = size;
+            mInvBindPose.TrimExcess();
+
+            for (int i = 0; i < size; ++i)
+            {
+                Transform world = mBindPose.GetGlobalTransform(i);
+                mInvBindPose[i] = Matrix.Invert(Transform.TransformToMatrix(world));
+            }
+        }
+    }
+}
