57a1345c1bf6aa79e5a94590a81617aaaca98239
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-18 12:19:21 -0300

    clip class integration

--- 3dAnimation/UmbrellaToolsKit.3D.Animation/Clip.cs
+++ 3dAnimation/UmbrellaToolsKit.3D.Animation/Clip.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UmbrellaToolsKit.Animation3D
 {
@@ -50,6 +47,92 @@ namespace UmbrellaToolsKit.Animation3D
 
             return inTime;
         }
+
+        public float Sample(Pose pose, float time)
+        {
+            if (GetDuration() == 0.0f)
+                return 0.0f;
+
+            time = AdjustTimeToFitRange(time);
+            int size = mTracks.Count();
+            for (int i = 0; i < size; ++i)
+            {
+                int j = mTracks[i].GetID();
+                Transform local = pose.GetLocalTransform(j);
+                Transform animated = mTracks[i].Sample(local, time, mLooping);
+                pose.SetLocalTransform(j, animated);
+            }
+
+            return time;
+        }
+
+        public void RecalculateDuration()
+        {
+            mStartTime = 0.0f;
+            mEndTime = 0.0f;
+            bool startSet = false;
+            bool endSet = false;
+            int tracksSize = mTracks.Count();
+            for (int i = 0; i < tracksSize; ++i)
+            {
+                if (mTracks[i].IsValid())
+                {
+                    float startTime = mTracks[i].GetStartTime();
+                    float endTime = mTracks[i].GetEndTime();
+
+                    if (startTime < mStartTime || !startSet)
+                    {
+                        mStartTime = startTime;
+                        startSet = true;
+                    }
+
+                    if (endTime > mEndTime || !endSet)
+                    {
+                        mEndTime = endTime;
+                        endSet = true;
+                    }
+                }
+            }
+        }
+
+        public TransformTrack this[int joint]
+        {
+            get
+            {
+                for (int i = 0, s = mTracks.Count(); i < s; ++i)
+                {
+                    if (mTracks[i].GetID() == joint)
+                    {
+                        return mTracks[i];
+                    }
+                }
+
+                mTracks.Add(new TransformTrack());
+                mTracks[mTracks.Count() - 1].SetID(joint);
+
+                return mTracks[mTracks.Count() - 1];
+            }
+        }
+
+        public string GetName() =>  mName;
+
+        public int GetIdAtIndex(int index) =>  mTracks[index].GetID();
+
+        public int Size() => (int)mTracks.Count();
+
+        public float GetDuration() =>  mEndTime - mStartTime;
+
+        public float GetStartTime() => mStartTime;
+
+        public float GetEndTime() =>  mEndTime;
+
+        public bool GetLooping() =>  mLooping;
+
+        public void SetName(string inNewName) =>  mName = inNewName;
+
+        public void SetIdAtIndex(int index, int id) =>  mTracks[index].SetID(id);
+
+        public void SetLooping(bool inLooping) => mLooping = inLooping;
 
     }
 }
