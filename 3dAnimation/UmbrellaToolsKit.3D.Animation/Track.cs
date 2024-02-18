ea3126ff7b01e1d584148a70d8b32a1094c7e25d
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-18 17:28:14 -0300

    integration of types of track

--- 3dAnimation/UmbrellaToolsKit.3D.Animation/Track.cs
+++ 3dAnimation/UmbrellaToolsKit.3D.Animation/Track.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace UmbrellaToolsKit.Animation3D
 {
@@ -9,21 +10,108 @@ namespace UmbrellaToolsKit.Animation3D
 
         public List<Frame> Frame => mFrames;
 
+        public Track() => mInterpolation = Interpolation.Linear;
 
-        public abstract void Resize(int size);
+        public Frame this[int index] => mFrames[index];
+
+        public void Resize(int size)
+        {
+            mFrames.Capacity = size;
+            mFrames.TrimExcess();
+        }
+
         public int Size() => mFrames.Count;
-        public Interpolation GetInterpolation() =>  mInterpolation;
-        public void SetInterpolation(Interpolation interpolation) => mInterpolation = interpolation;
-        public float GetStartTime() => mFrames.Count;
-        public float GetEndTime() =>  mFrames.Count;
-        public abstract T Sample(float time, bool looping);
 
-        protected abstract T SampleConstant(float time, bool looping);
+        public Interpolation GetInterpolation() =>  mInterpolation;
+        
+        public void SetInterpolation(Interpolation interpolation) => mInterpolation = interpolation;
+        
+        public float GetStartTime() => mFrames[0].mTime;
+        
+        public float GetEndTime() => mFrames[mFrames.Count - 1].mTime;
+        
+        public T Sample(float time, bool looping)
+        {
+            if (mInterpolation == Interpolation.Constant)
+                return SampleConstant(time, looping);
+            else if (mInterpolation == Interpolation.Linear)
+                return SampleLinear(time, looping);
+            return SampleCubic(time, looping);
+        }
+
+        protected T SampleConstant(float time, bool looping)
+        {
+            int frame = FrameIndex(time, looping);
+            if (frame < 0 || frame >= (int)mFrames.Count)
+                return (T)Convert.ChangeType(null, typeof(T));
+
+            return Cast(mFrames[frame].mValue);
+        }
+
+        protected float AdjustTimeToFitTrack(float time, bool looping)
+        {
+            int size = (int)mFrames.Count;
+            if (size <= 1) { return 0.0f; }
+
+            float startTime = mFrames[0].mTime;
+            float endTime = mFrames[size - 1].mTime;
+            float duration = endTime - startTime;
+            if (duration <= 0.0f) { return 0.0f; }
+            if (looping)
+            {
+                time = time - startTime % endTime - startTime;
+                if (time < 0.0f)
+                {
+                    time += endTime - startTime;
+                }
+                time = time + startTime;
+            }
+            else
+            {
+                if (time <= mFrames[0].mTime) { time = startTime; }
+                if (time >= mFrames[size - 1].mTime) { time = endTime; }
+            }
+
+            return time;
+        }
+
+        protected int FrameIndex(float time, bool looping)
+        {
+            int size = mFrames.Count;
+            if (size <= 1) return -1;
+
+            if (looping)
+            {
+                float startTime = mFrames[0].mTime;
+                float endTime = mFrames[size - 1].mTime;
+                float duration = endTime - startTime;
+
+                time = time - startTime % endTime - startTime;
+                if (time < 0.0f)
+                {
+                    time += endTime - startTime;
+                }
+                time = time + startTime;
+            }
+            else
+            {
+                if (time <= mFrames[0].mTime)
+                    return 0;
+
+                if (time >= mFrames[size - 2].mTime)
+                    return (int)size - 2;
+            }
+
+            for (int i = (int)size - 1; i >= 0; --i)
+                if (time >= mFrames[i].mTime)
+                    return i;
+            // Invalid code, we should not reach here!
+            return -1;
+        }
+
         protected abstract T SampleLinear(float time, bool looping);
         protected abstract T SampleCubic(float time, bool looping);
         protected abstract T Hermite(float time, T p1, T s1, T p2, T s2);
-        protected abstract int FrameIndex(float time, bool looping);
-        protected abstract float AdjustTimeToFitTrack(float time, bool looping);
-        protected abstract T Cast(float value);
+        protected abstract T Cast(float[] value);
     }
 }
--- 3dAnimation/UmbrellaToolsKit.3D.Animation/Tracks/ScalarTrack.cs
+++ 3dAnimation/UmbrellaToolsKit.3D.Animation/Tracks/ScalarTrack.cs
@@ -1,52 +1,72 @@
-﻿using System;
-
-namespace UmbrellaToolsKit.Animation3D.Tracks
+﻿namespace UmbrellaToolsKit.Animation3D.Tracks
 {
     public class ScalarTrack : Track<float>
     {
-        public override void Resize(int size)
-        {
-            throw new NotImplementedException();
-        }
+     
+        protected override float Cast(float[] value) => value[0];
 
-        public override float Sample(float time, bool looping)
+        protected override float Hermite(float t, float p1, float s1, float _p2, float s2)
         {
-            throw new NotImplementedException();
-        }
+            float tt = t * t;
+            float ttt = tt * t;
 
-        protected override float AdjustTimeToFitTrack(float time, bool looping)
-        {
-            throw new NotImplementedException();
-        }
+            float p2 = _p2;
+            TrackHelpers.Neighborhood(p1, p2);
 
-        protected override float Cast(float value)
-        {
-            throw new NotImplementedException();
-        }
+            float h1 = 2.0f * ttt - 3.0f * tt + 1.0f;
+            float h2 = -2.0f * ttt + 3.0f * tt;
+            float h3 = ttt - 2.0f * tt + t;
+            float h4 = ttt - tt;
 
-        protected override int FrameIndex(float time, bool looping)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override float Hermite(float time, float p1, float s1, float p2, float s2)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override float SampleConstant(float time, bool looping)
-        {
-            throw new NotImplementedException();
+            float result = p1 * h1 + p2 * h2 + s1 * h3 + s2 * h4;
+            return TrackHelpers.AdjustHermiteResult(result);
         }
 
         protected override float SampleCubic(float time, bool looping)
         {
-            throw new NotImplementedException();
+            int thisFrame = FrameIndex(time, looping);
+            if (thisFrame < 0 || thisFrame >= (int)(mFrames.Count - 1))
+                return 0.0f;
+
+            int nextFrame = thisFrame + 1;
+
+            float trackTime = AdjustTimeToFitTrack(time, looping);
+            float frameDelta = mFrames[nextFrame].mTime - mFrames[thisFrame].mTime;
+            if (frameDelta <= 0.0f)
+                return 0.0f;
+
+            float t = (trackTime - mFrames[thisFrame].mTime) / frameDelta;
+
+            float point1 = Cast(mFrames[thisFrame].mValue);
+            float slope1 = mFrames[thisFrame].mOut[0] * frameDelta;
+            slope1 = slope1 * frameDelta;
+
+            float point2 = Cast(mFrames[nextFrame].mValue);
+            float slope2 = mFrames[nextFrame].mIn[0] * frameDelta;
+            slope2 = slope2 * frameDelta;
+
+            return Hermite(t, point1, slope1, point2, slope2);
         }
 
         protected override float SampleLinear(float time, bool looping)
         {
-            throw new NotImplementedException();
+            int thisFrame = FrameIndex(time, looping);
+            if (thisFrame < 0 || thisFrame >= (int)(mFrames.Count - 1))
+                return 0.0f;
+
+            int nextFrame = thisFrame + 1;
+
+            float trackTime = AdjustTimeToFitTrack(time, looping);
+            float frameDelta = mFrames[nextFrame].mTime - mFrames[thisFrame].mTime;
+            if (frameDelta <= 0.0f)
+                return 0.0f;
+
+            float t = (trackTime - mFrames[thisFrame].mTime) / frameDelta;
+
+            float start = Cast(mFrames[thisFrame].mValue);
+            float end = Cast(mFrames[nextFrame].mValue);
+
+            return TrackHelpers.Interpolate(start, end, t);
         }
     }
 }
