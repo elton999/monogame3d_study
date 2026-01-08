082236e212e63b2dcfacaea0ecf0abc1a7757362
Elton Silva <eltoncarsilva73@gmail.com>
2026-01-08 18:03:17 -0300

    created class skeleton

---
+++ 3dMonoGameAnimation/3dAnimation/Skeleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _3dAnimation;
+
+public class Skeleton
+{
+    private Joint[] _joins = new Joint[] { };
+
+    public Joint[] Joins => _joins.ToArray();
+
+    public Skeleton() { }
+    
+    public Skeleton(List<Joint> joins)
+    {
+        _joins = joins.ToArray();
+    }
+
+    public Skeleton(Joint[] joins)
+    {
+        _joins = joins;
+    }
+}
