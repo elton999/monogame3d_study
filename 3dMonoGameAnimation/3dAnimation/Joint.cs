294566bd3aa3b0f16f0004a38e15cfcff9602d59
Elton Silva <eltoncarsilva73@gmail.com>
2026-01-08 18:02:54 -0300

    created joint class

---
+++ 3dMonoGameAnimation/3dAnimation/Joint.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace _3dAnimation;
+public class Joint
+{
+    private Joint[] _parents = [];
+    private string _name;
+    private float _weight;
+
+    public bool HasParent => _parents.Length > 0;
+    public Joint[] Parents => _parents;
+    public string Name => _name;
+    public float Weight => _weight;
+
+    public Joint(string name, float weight)
+    { 
+        _name = name;
+        _weight = weight;
+    }
+
+    public void SetParent(Joint parent)
+    {
+        var tempParents = _parents.ToList();
+        
+        if (tempParents.Contains(parent)) return;
+        
+        tempParents.Add(parent);
+        _parents = tempParents.ToArray();
+    }
+}
